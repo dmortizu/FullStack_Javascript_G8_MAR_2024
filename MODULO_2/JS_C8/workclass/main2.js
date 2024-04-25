3ce473dfeca9da8d1548164d8dc712cc44c687ce
Daniel Canon <danielycm02@gmail.com>
2024-04-24 21:37:52 -0500

    clase_8

--- MODULO_2/JS_C8/workclass/main2.js
+++ MODULO_2/JS_C8/workclass/main2.js
@@ -8,6 +8,11 @@ function calcImc (peso, altura){
     return result;
 }
 
+// Las variables dentro de una funcion solo existen dentro de ellas;
+// no se puede usar afuera de ella 
+
+// Lo que se coloque despues del return no se ejecuta
+
 const imc = calcImc(50, 20);
 console.log("IMC", imc)
 
