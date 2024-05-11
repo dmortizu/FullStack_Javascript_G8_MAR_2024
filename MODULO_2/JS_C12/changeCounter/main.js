50290d26dd418b68d65207c6ac32306ce22a6033
Daniel Canon <danielycm02@gmail.com>
2024-05-10 20:37:50 -0500

    clase_12

---
+++ MODULO_2/JS_C12/changeCounter/main.js
@@ -0,0 +1,15 @@
+const counterElement = document.getElementById("counter");
+
+let count = 0
+const incrementar = () => {
+    count = count + 1;
+    counterElement.innerHTML = count;
+}
+
+// Primer click = count => 1
+// Segundo click count =>  2
+// Tercer click count => 3 
+
+const decrementar = () => {
+
+}
