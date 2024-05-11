f5d72cdce2082263d646cb7d9e1e259c186d4b63
Daniel Canon <danielycm02@gmail.com>
2024-05-10 20:17:58 -0500

    clase_12

---
+++ MODULO_2/JS_C12/changeText/main.js
@@ -0,0 +1,7 @@
+// document.write("Hola mundo")
+
+const parrafo = document.getElementById("text1");
+
+function cambiarTexto() {
+    parrafo.innerHTML = "Este parrafo se modifico por medio de JavaScript"
+}
